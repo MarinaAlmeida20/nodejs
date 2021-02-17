056278368ac8c0a1a43056f40c288709292a7a37
Marina <marinabrito.ma@gmail.com>
2021-02-17 18:29:47 +0000

    mysql

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,12 +1,17 @@
 const express = require('express'); // Call modules 'express' is a library(biblioteca)
 const consign = require('consign'); // import consign
 
+const bodyParser = require('body-parser'); // função é justamente converter as requisições para algo que seja legível no JavaScript
+
 module.exports = () => { // function do something(configura o app)
     const app = express() // to execute the app
 
+    app.use(bodyParser.urlencoded({extended: true})) // biblioteca específica to appear the name:marina no browser
+    app.use(bodyParser.json()) // To frontEnd send to backEnd information
+
     consign() // include the controller inside the app
-    .include('controllers')
-    .into(app)
+        .include('controllers')
+        .into(app)
 
     return app;
 };
--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,9 @@
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Voce esta na rota de atendimentos estarealizando um GET'))
+    app.get('/atendimentos', (req, res) => res.send('Voce esta na rota de atendimentos esta realizando um GET')) // get date (pegar dados)
+
+    app.post('/atendimentos', (req, res) => {
+
+            console.log(req.body)
+            res.send('Voce esta na rota de atendimento e esta realizando um POST')
+        }) // Post
 }
