f2245edae86627917e517b8da9478fc1e4a6d633
Marina <marinabrito.ma@gmail.com>
2021-02-17 22:50:01 +0000

    creating tables

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const customExpress = require('./config/customExpress'); // export the module.ex
 
 const conexao = require('./infraestrutura/conexao'); // export the module conexao
 
+const Tabelas = require('./infraestrutura/tabelas');
+
 conexao.connect(erro => { // try to connect
     if(erro) {
         console.log(erro)
     } else {
         console.log('Conectado com sucesso')
 
-        const app = customExpress() // running the function
+        Tabelas.init(conexao);
+        const app = customExpress(); // running the function
 
         app.listen(3000, () => console.log('Server running in the door 3000'));
-    }
-})
+        
+    } 
+});
 
 
 // when someone go to door 3000, will happening something.
