6d1d0c2aa8fbe9b8723b55260cc85fc9c5cc74af
Marina <marinabrito.ma@gmail.com>
2021-02-17 21:20:20 +0000

    conexao

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const customExpress = require('./config/customExpress'); // export the module.exports ( function)
 
+const conexao = require('./infraestrutura/conexao'); // export the module conexao
 
-const app = customExpress() // running the function
+conexao.connect(erro => { // try to connect
+    if(erro) {
+        console.log(erro)
+    } else {
+        console.log('Conectado com sucesso')
 
-app.listen(3000, () => console.log('Server running in the door 3000'));
+        const app = customExpress() // running the function
+
+        app.listen(3000, () => console.log('Server running in the door 3000'));
+    }
+})
+
+
 // when someone go to door 3000, will happening something.
 
 // app.get('/', (req, res) => res.send('Server running, everything ok'))
--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql'); // import mysql
 
 const conexao = mysql.createConnection({
-    host: '127.0.0.1',
+    host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 1625,
+    password: 'novasenha',
     database: 'agenda-petshop'
 })
 
